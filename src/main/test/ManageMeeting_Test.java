24cd7c4a885f3e0446a5e09388b64774336ab313
EmanuelDuca <emanuel.duca2000@gmail.com>
2023-05-03 11:36:31 +0200

    addMeating Test

--- src/main/test/ManageMeeting_Test.java
+++ src/main/test/ManageMeeting_Test.java
@@ -1,3 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+//import org.mockito.Mockito;
+import static org.junit.jupiter.api.Assertions.*;
 public class ManageMeeting_Test
 {
+  @Test
+  void a_new_meetingList_has_zero_meetings(){
+    assertTrue();
+  }
 }
