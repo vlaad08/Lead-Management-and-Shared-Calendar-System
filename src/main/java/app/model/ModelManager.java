49f668d202cdcd2680d5ee2824ad87fd7aad3397
EmanuelDuca <emanuel.duca2000@gmail.com>
2023-05-03 11:13:02 +0200

    Changes in the code
    Model, ModelManager, MeatingView and MeetingController

--- src/main/java/app/model/ModelManager.java
+++ src/main/java/app/model/ModelManager.java
@@ -1,6 +1,32 @@
 package app.model;
 
+import app.shared.Meeting;
+
+import java.util.ArrayList;
+import java.util.Date;
 
 public class ModelManager implements Model
 {
+  @Override public void addMeeting(Date startDate, Date endDate,
+      String description, ArrayList<Employee> employees)
+  {
+
+  }
+
+  @Override public void removeMeeting(Meeting meeting)
+  {
+
+  }
+
+  @Override public ArrayList<Meeting> getMeetings()
+  {
+    return null;
+  }
+
+  @Override public void editMeeting(Date oldStartDate, Date oldEndDate,
+      Date startDate, Date endDate, String description,
+      ArrayList<Employee> employees)
+  {
+
+  }
 }
