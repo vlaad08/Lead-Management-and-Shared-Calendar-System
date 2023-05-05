d303cb894162322d0e5c214954b86bde00d808f2
vlaad08 <62983956+vlaad08@users.noreply.github.com>
2023-05-05 10:27:50 +0200

    v

--- src/main/java/app/TestClass.java
+++ src/main/java/app/TestClass.java
@@ -1,21 +1,25 @@
 package app;
 
 import app.JDBC.SQLConnection;
+import app.shared.Meeting;
 
 import java.sql.Date;
 import java.sql.SQLException;
 import java.sql.Time;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.ArrayList;
 
 public class TestClass
 {
   public static void main(String[] args) throws SQLException
   {
     SQLConnection sqlConnection = SQLConnection.getInstance();
-    sqlConnection.createMeetingInBusiness("Firing somebody", "Get ready for us to fire an employee, the one with no value to the business",
-        Date.valueOf(LocalDate.of(2023,6,5)),
-        Time.valueOf(LocalTime.of(10,30,0)),
-        Time.valueOf(LocalTime.of(10,45,0)), 2);
+    ArrayList<Meeting> meetings = sqlConnection.getMeetingsByBusinessId(1);
+    for(Meeting meeting : meetings)
+    {
+      System.out.println(meeting);
+    }
+
   }
 }
